a894d770aadeaaca1d9e480c662f384f93f11a62
DadoPolumentol <davidmts123123@gmail.com>
2025-06-26 23:13:24 +0200

    glupost

--- micspam.py
+++ micspam.py
@@ -1,2 +1,22 @@
+import os
 import pygame
 pygame.mixer.init()
+
+high_power_mode=bool(None)
+audio_num=None
+
+files=os.listdir("./audio")
+number_list=[None]
+for f in files:
+    x=0
+    for a in f:
+        is_digit=0
+        print(f[x])
+        is_digit=f[x].isdigit()
+        if is_digit != True:
+            break
+        number_list.append(x)
+        x+=1
+
+for list in number_list:
+    print(list)
